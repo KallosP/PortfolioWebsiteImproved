655867e320c210b134087ce132cc86b33f44d8a6
KallosP <pkallos19@gmail.com>
2024-12-28 14:09:45 -0800

    locally scoped css styling for components with modules

--- packages/react-frontend/src/components/Home.jsx
+++ packages/react-frontend/src/components/Home.jsx
@@ -1,32 +1,47 @@
-import '../styles/home.css'
+import styles from '../styles/home.module.css'
 
 function Home() {
+	const fluidContainerDivClasses = [
+		'container-fluid',
+		'd-flex',
+		'flex-column',
+		'px-5',
+		'border',
+		'align-items-start',
+		styles['c-div-content']
+	].join(' ')
+	const h2Classes = ['text-center', 'text-secondary'].join(' ')
+	const pClasses = ['text-center', 'text-secondary', styles['c-home-p']].join(' ')
+	const ulClasses = ['list-unstyled', 'text-center'].join(' ')
+	const liClasses = ['d-inline-block', 'mx-2'].join(' ')
+	const buttonClasses = ['btn', 'btn-primary'].join(' ')
+
 	return (
-		<div class="container-fluid d-flex flex-column px-5 border align-items-start c-div-content">
-			<h2 class="text-center text-secondary">Peter Kallos</h2>
-			<p class="text-center text-secondary c-home-p">
+		<div class={fluidContainerDivClasses}>
+			<h2 class={h2Classes}>Peter Kallos</h2>
+			<p class={pClasses}>
 				I'm a fourth-year Computer Science student at Cal Poly, San Luis Obispo, with a strong
 				interest in full-stack development and a particular focus on mobile apps. Explore my work
 				and learn more about me below.
 			</p>
-			<ul class="list-unstyled text-center">
-				<li class="d-inline-block mx-2">
+			<ul class={ulClasses}>
+				<li class={liClasses}>
 					<a href="https://github.com/KallosP" target="_blank">
 						<button>GitHub</button>
 					</a>
 				</li>
-				<li className="d-inline-block mx-2">
+				<li className={liClasses}>
 					<a href="https://www.linkedin.com/in/peter-kallos/" target="_blank">
 						<button>LinkedIn</button>
 					</a>
 				</li>
-				<li className="d-inline-block mx-2">
+				<li className={liClasses}>
 					<a href="mailto:pkallos19@gmail.com">
 						<button>Email</button>
 					</a>
 				</li>
 			</ul>
-			<button type="button" class="btn btn-primary">
+			<button type="button" class={buttonClasses}>
 				Base class
 			</button>
 		</div>
--- packages/react-frontend/src/components/Navbar.jsx
+++ packages/react-frontend/src/components/Navbar.jsx
@@ -1,13 +1,31 @@
 //import React from 'react'
-import '../styles/navbar.css'
+import styles from '../styles/navbar.module.css'
 import closeIcon from '../assets/svg-icons/close.svg'
 
 function Navbar() {
+	const navClasses = [
+		'navbar',
+		'fixed-top',
+		'navbar-expand-lg',
+		'navbar-light',
+		'mx-3',
+		'my-3',
+		styles['c-main-nav']
+	].join(' ')
+	const containerDivClasses = ['container-fluid', 'justify-content-center'].join(' ')
+	const navbarTogglerClasses = ['navbar-toggler', styles['c-navbar-toggler']].join(' ')
+	const navbarCollapseClasses = ['collapse', 'navbar-collapse'].join(' ')
+	const navbarNavClasses = ['navbar-nav', 'align-items-center', styles['c-navbar-nav']].join(' ')
+	const navItemFirstLiClasses = ['nav-item', styles['c-nav-item'], styles['c-first-li']].join(' ')
+	const navItemClasses = ['nav-item', styles['c-nav-item']].join(' ')
+	const navItemLastLiClasses = ['nav-item', styles['c-nav-item'], styles['c-last-li']].join(' ')
+	const navLinkClasses = ['nav-link', styles['c-nav-link']].join(' ')
+
 	return (
-		<nav className="navbar fixed-top navbar-expand-lg navbar-light mx-3 my-3 c-main-nav">
-			<div className="container-fluid justify-content-center">
+		<nav className={navClasses}>
+			<div className={containerDivClasses}>
 				<button
-					className="navbar-toggler"
+					className={navbarTogglerClasses}
 					type="button"
 					data-bs-toggle="collapse"
 					data-bs-target="#navbarNav"
@@ -16,30 +34,30 @@ function Navbar() {
 					aria-label="Toggle navigation">
 					<img src={closeIcon} alt="Close" />
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav align-items-center">
-						<li className="nav-item first-li">
-							<a className="nav-link" aria-current="page" href="#">
+				<div className={navbarCollapseClasses} id="navbarNav">
+					<ul className={navbarNavClasses}>
+						<li className={navItemFirstLiClasses}>
+							<a className={navLinkClasses} aria-current="page" href="#">
 								Home
 							</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
+						<li className={navItemClasses}>
+							<a className={navLinkClasses} href="#">
 								Projects
 							</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
+						<li className={navItemClasses}>
+							<a className={navLinkClasses} href="#">
 								Skills
 							</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
+						<li className={navItemClasses}>
+							<a className={navLinkClasses} href="#">
 								Education
 							</a>
 						</li>
-						<li className="nav-item last-li">
-							<a className="nav-link" href="#">
+						<li className={navItemLastLiClasses}>
+							<a className={navLinkClasses} href="#">
 								Contact
 							</a>
 						</li>
