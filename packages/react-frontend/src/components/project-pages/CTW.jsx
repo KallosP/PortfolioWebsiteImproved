40745ad6a9973860b11feed168cdc6749ad129b7
KallosP <pkallos19@gmail.com>
2025-01-10 20:40:33 -0800

    started on project page general layout, designed back button to be same as navbar on home page

--- packages/react-frontend/src/components/project-pages/CTW.jsx
+++ packages/react-frontend/src/components/project-pages/CTW.jsx
@@ -1,16 +1,40 @@
 import styles from '../../styles/ctw-page.module.css'
 import prevArrowIcon from '../../assets/svg-icons/prev-arrow.svg'
+import CTWGraphic from '../../assets/images/Feature_Graphic.png'
 import { Link } from 'react-router-dom'
 
 function ProjectPage() {
-	const fluidContainerDivClasses = ['container-fluid', styles['c-fluid-container']].join(' ')
-	const backButtonClasses = [styles['c-back-button']].join(' ')
+	const fluidContainerDivClasses = [
+		'container-fluid',
+		'col',
+		'text-center',
+		styles['c-fluid-container']
+	].join(' ')
+	const backButtonContainerClasses = [
+		'fixed-top',
+		'mx-3',
+		'my-3',
+		styles['c-back-button-container']
+	].join(' ')
+	const headerImageContainerClasses = ['row',styles['c-header-image-container']].join(' ')
+	const headerImageClasses = [styles['c-header-image']].join(' ')
+	const infoContainerClasses = ['row', styles['c-info-container']].join(' ')
 
 	return (
 		<div className={fluidContainerDivClasses}>
-			<Link to="/" className={backButtonClasses}>
-				<img src={prevArrowIcon} />
-			</Link>
+			<div className={backButtonContainerClasses}>
+				<Link to="/" className={styles['c-back-button']}>
+					Back
+				</Link>
+			</div>
+
+			<div className={headerImageContainerClasses}>
+				<img src={CTWGraphic} className={headerImageClasses}></img>
+			</div>
+
+			<div className={infoContainerClasses}>
+				<p className={'text-secondary'}>Test</p>
+			</div>
 		</div>
 	)
 }
